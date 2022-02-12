7eac8c1cfe85c0d2f9d3ccb460777c85903b0b45
Nikolai <nikolamoskale@gmail.com>
2022-02-12 11:07:29 +0300

    Работа с роутами , создание общего шаблона и плдключение через секции

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('shablon')
+
+@section('title')
+Главная страница
+@endsection
+
+@section('main_content')
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci sint id suscipit rerum. At consequatur voluptatem rerum vitae repudiandae laborum facilis magnam cumque, obcaecati minima dicta vel mollitia ut assumenda.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-Route::get('/contact', function () {
+Route::get('/contact/{id}', function ($id) {
     return view('contact');
 });
